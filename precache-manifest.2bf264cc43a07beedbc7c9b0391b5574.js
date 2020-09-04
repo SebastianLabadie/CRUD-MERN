7283715d268a33c114a85b66fe164a579c40694c
SebastianLabadie <sebastianlabadie.contact@gmail.com>
2020-09-04 00:50:37 -0300

    Updates

---
+++ precache-manifest.2bf264cc43a07beedbc7c9b0391b5574.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e396887f90fbe50af3f49b94f95e0cd9",
+    "url": "/CRUD-MERN/index.html"
+  },
+  {
+    "revision": "4984d5d7adf26f179536",
+    "url": "/CRUD-MERN/static/css/2.b56a0318.chunk.css"
+  },
+  {
+    "revision": "9573dc138197bb448fce",
+    "url": "/CRUD-MERN/static/css/main.d3aeb551.chunk.css"
+  },
+  {
+    "revision": "4984d5d7adf26f179536",
+    "url": "/CRUD-MERN/static/js/2.e21ccbec.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/CRUD-MERN/static/js/2.e21ccbec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9573dc138197bb448fce",
+    "url": "/CRUD-MERN/static/js/main.5c7a565a.chunk.js"
+  },
+  {
+    "revision": "8c9819ba2d619396bd04",
+    "url": "/CRUD-MERN/static/js/runtime-main.492d570e.js"
+  }
+]);
